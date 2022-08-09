3890d137b5474e2c755302d63c282fa7cbec24d5
Ratnayash-Pandey <87600726+Ratnayash-Pandey@users.noreply.github.com>
2022-08-09 14:58:30 +0530

    Update Day 5-2.js
    This is the one accepted there

--- Day 5/Day 5-2.js
+++ Day 5/Day 5-2.js
@@ -1,3 +1,5 @@
-const PI = Math.PI;
-const EULER = Math.E;
-const SQRT2 = Math.SQRT2;
+const PI = Math.PI, EULER = Math.E, SQRT2 = Math.SQRT2;
+console.log(EULER);
+console.log(PI);
+console.log(SQRT2);
+return 0;
